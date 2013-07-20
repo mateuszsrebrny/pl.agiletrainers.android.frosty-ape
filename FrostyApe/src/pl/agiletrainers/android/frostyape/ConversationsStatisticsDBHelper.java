a972e7b57d612c33d9620b89e136afa696b53da6
mateusz <mateusz@agiletrainers.pl>
2013-07-20 19:19:26 +0200

    better create statement

--- FrostyApe/src/pl/agiletrainers/android/frostyape/ConversationsStatisticsDBHelper.java
+++ FrostyApe/src/pl/agiletrainers/android/frostyape/ConversationsStatisticsDBHelper.java
@@ -7,14 +7,19 @@ public class ConversationsStatisticsDBHelper extends SQLiteOpenHelper
 {
 	public static String DATABASE_NAME = "ConversationsStatisticsDB";
 
-	private static String TABLE_NAME = "comversations_statistics";
+	private static String TABLE_NAME = "conversations_statistics";
+	
+	private static String COLUMN_ID = "_id";
+	private static String COLUMN_NUM_CONV = "num_conversations";
+	private static String COLUMN_NUM_UNREAD_CONV = "num_unread_converdations";
+	private static String COLUMN_TIME_POINT = "time_point";
 	
 	public static String CREATE_SQL = 
-	    "CREATE TABLE IF NOT EXISTS " + TABLE_NAME
-		+ "( id INTEGER PRIMARY KEY AUTOINCREMENT, "
-		+ "time_point DATETIME, "
-		+ "num_conversations INT, "
-	+ "num_unread_conversations INT)";
+	    "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " ( "
+		+ COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+		+ COLUMN_TIME_POINT + " DATETIME, "
+		+ COLUMN_NUM_CONV + " INT, "
+    	+ COLUMN_NUM_UNREAD_CONV + " INT)";
 	
 	public ConversationsStatisticsDBHelper(Context context) {
 		super(context, DATABASE_NAME, null, 33);
