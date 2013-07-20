2e8812af92d9a1c4e2b3cc1da785aff54527dd39
mateusz <mateusz@agiletrainers.pl>
2013-07-20 13:19:36 +0200

    create table statement

--- FrostyApe/src/pl/agiletrainers/android/frostyape/ConversationsStatisticsDBHelper.java
+++ FrostyApe/src/pl/agiletrainers/android/frostyape/ConversationsStatisticsDBHelper.java
@@ -7,6 +7,15 @@ public class ConversationsStatisticsDBHelper extends SQLiteOpenHelper
 {
 	public static String DATABASE_NAME = "ConversationsStatisticsDB";
 
+	private static String TABLE_NAME = "comversations_statistics";
+	
+	public static String CREATE_SQL = 
+	    "CREATE TABLE IF NOT EXISTS " + TABLE_NAME
+		+ "( id INTEGER PRIMARY KEY AUTOINCREMENT, "
+		+ "time_point DATETIME, "
+		+ "num_conversations INT, "
+	+ "num_unread_conversations INT)";
+	
 	public ConversationsStatisticsDBHelper(Context context) {
 		super(context, DATABASE_NAME, null, 33);
 	}
