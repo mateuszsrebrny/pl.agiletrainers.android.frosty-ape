4290e04db7f517142ab910a59ec599a201d957ec
Mateusz Srebrny <mateusz@agiletrainers.pl>
2013-07-22 20:39:04 +0200

    first widget tries

---
+++ FrostyApe/src/pl/agiletrainers/android/frostyape/WidgetProvider.java
@@ -0,0 +1,14 @@
+package pl.agiletrainers.android.frostyape;
+
+import android.appwidget.*;
+import android.content.*;
+
+public class WidgetProvider extends AppWidgetProvider {
+
+    @Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] apoWidgetIds) {
+		
+	}
+	
+	
+}
