53035cac49c76be692520b71a704d6efa2ec1a1d
Mateusz Srebrny <mateusz@agiletrainers. pl>
2013-07-23 19:06:46 +0200

    widget

--- FrostyApe/src/pl/agiletrainers/android/frostyape/WidgetProvider.java
+++ FrostyApe/src/pl/agiletrainers/android/frostyape/WidgetProvider.java
@@ -2,12 +2,20 @@ package pl.agiletrainers.android.frostyape;
 
 import android.appwidget.*;
 import android.content.*;
+import android.widget.*;
 
 public class WidgetProvider extends AppWidgetProvider {
 
     @Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] apoWidgetIds) {
 		
+		ComponentName thisWidget = new ComponentName(context, WidgetProvider.class);
+		int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+		
+		for (int widgetId : allWidgetIds) {
+			RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
+			appWidgetManager.updateAppWidget(widgetId, remoteViews);
+		}
 	}
 	
 	
