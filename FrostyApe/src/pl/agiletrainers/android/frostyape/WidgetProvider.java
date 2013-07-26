eaa70bb46eed812fc48558343c0031003fb3d957
Mateusz Srebrny <mateusz@agiletrainers.pl>
2013-07-26 13:41:23 +0300

    widget text changing :)

--- FrostyApe/src/pl/agiletrainers/android/frostyape/WidgetProvider.java
+++ FrostyApe/src/pl/agiletrainers/android/frostyape/WidgetProvider.java
@@ -7,15 +7,19 @@ import android.widget.*;
 public class WidgetProvider extends AppWidgetProvider {
 
     @Override
-	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] apoWidgetIds) {
-		
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		String textToDisplay = ":D ";
 		ComponentName thisWidget = new ComponentName(context, WidgetProvider.class);
 		int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
 		
 		for (int widgetId : allWidgetIds) {
+			textToDisplay += ", " + widgetId;
 			RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
+		
+			remoteViews.setTextViewText(R.id.widget_text_view, textToDisplay);
 			appWidgetManager.updateAppWidget(widgetId, remoteViews);
 		}
+	
 	}
 	
 	
