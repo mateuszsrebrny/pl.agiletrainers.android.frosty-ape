4ef4e0b15883b382867d3a7b02d439ab1fe755f9
Mateusz Srebrny <mateusz@agiletrainers.pl>
2013-08-09 21:04:57 +0200

    more data from db

--- FrostyApe/src/pl/agiletrainers/android/frostyape/ConversationsStatisticsDBHelper.java
+++ FrostyApe/src/pl/agiletrainers/android/frostyape/ConversationsStatisticsDBHelper.java
@@ -52,7 +52,7 @@ public class ConversationsStatisticsDBHelper extends SQLiteOpenHelper
 		
 		SQLiteDatabase db = this.getReadableDatabase();
 		
-		Cursor cursor = db.rawQuery("select * from " + TABLE_NAME + " order by " + COLUMN_TIME_POINT_MILIS + " desc limit 100", null);
+		Cursor cursor = db.rawQuery("select * from " + TABLE_NAME + " order by " + COLUMN_TIME_POINT_MILIS + " desc limit 150", null);
 		
 		cursor.moveToFirst();
 		
